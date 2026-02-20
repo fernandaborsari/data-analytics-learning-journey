94aa9bb05ff039b3bbef2137af1592aa0e476670
fernandaborsari <fernanda_borsari@hotmail.com>
2026-02-20 11:08:04 -0600

    Create explore_data.sql

---
+++ projects/academic_stress/sql/explore_data.sql
@@ -0,0 +1,12 @@
+-- View all data
+SELECT *
+FROM academic_stress;
+
+-- Count total rows
+SELECT COUNT(*) AS total_rows
+FROM academic_stress;
+
+-- Check missing values in each column (example)
+SELECT
+    COUNT(*) AS total_rows
+FROM academic_stress;
